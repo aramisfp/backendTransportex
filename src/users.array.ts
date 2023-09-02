ed87c6d6e93e45a00386b662d677d3b9f2b07795
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2023-09-02 14:36:02 -0600

    Update users.array.ts
    Se agrego a Transviza como cliente

--- src/users.array.ts
+++ src/users.array.ts
@@ -190,5 +190,13 @@ export const myArray = [
         Database:'DB_TRANSPORTEX',
         UserID:'usr_movil',
         UserPwd:'Tran5p0rt3X_2008tlf',
+    },  
+    {
+        name:'TRANSVIZA_PER',
+        Server:'transvizaperu.cxmdx8nbbnul.us-east-1.rds.amazonaws.com',
+        Port:'1433',
+        Database:'DB_TRANSPORTEX',
+        UserID:'usr_movil',
+        UserPwd:'Tran5p0rt3X_2008tlf',
     },     	  	
 ]

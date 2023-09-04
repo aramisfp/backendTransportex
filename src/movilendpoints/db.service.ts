6ecbe3fbcc715c506a88ed70422e7acb7315e05d
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2023-09-04 07:59:19 -0600

    Update db.service.ts
    Se agrego la funcion de Novedades al final

--- src/movilendpoints/db.service.ts
+++ src/movilendpoints/db.service.ts
@@ -135,4 +135,9 @@ export class DataService {
     let result = await this.general(object, query);
     return result;
   }
+  async novedades(object, ID_Empleado, todas){
+    const query = `select ID_Actividad_Novedad, Vehiculo_Identificador_Primario, Empleado_Solicitud, Fecha_Solicitud, Antiguedad_Dias, Empleado_Asignacion, Fecha_Asignacion, Fecha_Asignacion_EnvioEmail, Fecha_Atencion, Descripcion, Actividad_Grupo, Estado, Estatus, Prioridad, Tipo, Urgente, ID_Hoja_Revision, Referencia, Observaciones_Cierre, Fecha_Cierre, Fecha_Creacion, Fecha_Modificacion, Usuario_Creacion, Usuario_Modificacion, ID_Vehiculo, ID_Empleado_Solicitud, ID_Empleado_Asignacion, ID_Usuario_Solicitud, ID_Actividad_Grupo, Actividades_Asociadas, Empresa_Ambiente from dbo.F_SEL_ACTIVIDAD_SOLICITUD(${ID_Empleado},${todas})`;
+    let result = await this.general(object, query);
+    return result;
+  }   
 }

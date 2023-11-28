4ac7363d9b1597b0a611a419dfc25efb106ede90
JapCode <japcrowley.thetraveler@gmail.com>
2023-11-28 01:24:08 -0400

    test payloadTooLargeError

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000);
   // await app.listen(3001);
 }

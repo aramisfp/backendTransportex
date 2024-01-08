d3b3eb5e7030060bd4a30315119ad05cb0b76622
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2024-01-08 14:01:26 -0600

    Update users.array.ts
    Se quitaron conexiones de clientes que no han pagado el uso de la app movil

--- src/users.array.ts
+++ src/users.array.ts
@@ -15,20 +15,4 @@ export const myArray = [
     UserID: 'oscar',
     UserPwd: '12345678',
   },
-  {
-    name: 'HEFFIX_GUA',
-    Server: 'heffix.ckss2h21e4d9.us-east-1.rds.amazonaws.com',
-    Port: '1433',
-    Database: 'DB_TRANSPORTEX',
-    UserID: 'usr_movil',
-    UserPwd: 'Tran5p0rt3X_2008tlf',
-  },
-  {
-    name: 'LLP_CR',
-    Server: 'llantaspacifico.cwxyhuabjmmw.us-east-2.rds.amazonaws.com',
-    Port: '1433',
-    Database: 'DB_TRANSPORTEX',
-    UserID: 'usr_movil',
-    UserPwd: 'Tran5p0rt3X_2008tlf',
-  },
 ];

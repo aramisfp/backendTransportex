e5f8fc89177c33ee82e49ab4a5444499e3036703
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2024-01-26 08:32:01 -0600

    Update users.array.ts
    Conexion de nuevo cliente Madeco

--- src/users.array.ts
+++ src/users.array.ts
@@ -15,4 +15,12 @@ export const myArray = [
     UserID: 'oscar',
     UserPwd: '12345678',
   },
+  {
+    name: 'MADECO_MEX',
+    Server: 'madeco.cxwcckmgw667.us-east-2.rds.amazonaws.com',
+    Port: '1433',
+    Database: 'DB_TRANSPORTEX',
+    UserID: 'usr_movil',
+    UserPwd: 'Tran5p0rt3X_2008tlf',
+  },  
 ];

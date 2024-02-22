e904420215b42dbb98cc7979802bbbbc0069fff9
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2024-02-22 12:02:18 -0600

    Update users.array.ts
    Nuevo cliente Beraldi

--- src/users.array.ts
+++ src/users.array.ts
@@ -39,4 +39,12 @@ export const myArray = [
     UserID: 'usr_movil',
     UserPwd: 'Tran5p0rt3X_2008tlf',
   },      
+ {
+    name: 'BERALDI2_ARG',
+    Server: 'ec2-18-222-97-179.us-east-2.compute.amazonaws.com',
+    Port: '1433',
+    Database: 'DB_TRANSPORTEX',
+    UserID: 'usr_movil',
+    UserPwd: 'Tran5p0rt3X_2008tlf',
+  },        
 ];

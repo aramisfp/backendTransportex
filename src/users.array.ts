3ea466f0e9156b85fd6decc4b6dc3cc51178c020
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2024-02-26 17:09:50 -0600

    Update users.array.ts
    Se agrego otro cliente: CPS

--- src/users.array.ts
+++ src/users.array.ts
@@ -47,4 +47,12 @@ export const myArray = [
     UserID: 'usr_movil',
     UserPwd: 'Tran5p0rt3X_2008tlf',
   },        
+ {
+    name: 'CPS_PAN',
+    Server: 'cps.ccysvfpsgntm.us-east-2.rds.amazonaws.com',
+    Port: '1433',
+    Database: 'DB_TRANSPORTEX',
+    UserID: 'usr_movil',
+    UserPwd: 'Tran5p0rt3X_2008tlf',
+  },        
 ];

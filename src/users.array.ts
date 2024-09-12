e037bf661a585d37421e2b5640feef28d9b3f8a8
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2024-09-12 10:31:58 -0600

    Update users.array.ts
    Nuevo cliente Cable and Wireless

--- src/users.array.ts
+++ src/users.array.ts
@@ -86,5 +86,13 @@ export const myArray = [
     Database: 'DB_TRANSPORTEX_Logiexpress',
     UserID: 'usr_movil',
     UserPwd: 'Tran5p0rt3X_2008tlf',
-  },            
+  },      
+ {
+    name: 'CW_PAN',
+    Server: 'ec2-3-15-39-223.us-east-2.compute.amazonaws.com',
+    Port: '1433',
+    Database: 'DB_TRANSPORTEX',
+    UserID: 'usr_movil',
+    UserPwd: 'Tran5p0rt3X_2008tlf',
+  },              
 ];

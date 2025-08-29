1124d280eb16fa325f680bd02ff868967d41bd10
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2025-08-29 11:36:59 -0600

    Update db.service.ts
    Mejora del query de repostajeformapago

--- src/movilendpoints/db.service.ts
+++ src/movilendpoints/db.service.ts
@@ -319,10 +319,12 @@ order by 2`;
 select 'Depósito/consignación' as tipogasto_desc, 'D' as valor union all
 select 'Cheque' as tipogasto_desc, 'C' as valor union all
 select 'Efectivo' as tipogasto_desc, 'E' as valor union all
-select 'Prepagado' as tipogasto_desc, 'P' as valor from configuracion where configuracion.CAMPO = 'CODIGO_EMPRESA' and configuracion.VALOR <> 'BANCONAL_PAN' union all 
-select 'Otros' as tipogasto_desc, 'O' as valor union all
-select 'Tarjeta' as tipogasto_desc, 'A' as valor from configuracion where configuracion.CAMPO = 'CODIGO_EMPRESA' and configuracion.VALOR IN ('BANCONAL_PAN', 'ROBINSON_ARG', 'MESSINA_ARG') union all
-select 'Chip' as tipogasto_desc, 'B' as valor from configuracion where configuracion.CAMPO = 'CODIGO_EMPRESA' and configuracion.VALOR = 'BANCONAL_PAN'`;
+select 'Prepagado' as tipogasto_desc, 'P' as valor from configuracion where configuracion.CAMPO = 'CODIGO_EMPRESA' and 
+configuracion.VALOR <> 'BANCONAL_PAN' and charindex(configuracion.VALOR, '_ARG') = 0 union all 
+select 'Tarjeta' as tipogasto_desc, 'A' as valor from configuracion where configuracion.CAMPO = 'CODIGO_EMPRESA' and 
+(configuracion.VALOR = 'BANCONAL_PAN' or charindex(configuracion.VALOR, '_ARG') > 0) union all
+select 'Chip' as tipogasto_desc, 'B' as valor from configuracion where configuracion.CAMPO = 'CODIGO_EMPRESA' and configuracion.VALOR = 'BANCONAL_PAN' union all
+select 'Otros' as tipogasto_desc, 'O' as valor`;
     const result = await this.general(object, query);
     return result;
   }	

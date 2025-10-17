99c122de9f2c68b42637251e904280f291db132a
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2025-10-16 19:26:26 -0600

    Update db.service.ts
    Nuevo campo de uso_descripcion en el query

--- src/movilendpoints/db.service.ts
+++ src/movilendpoints/db.service.ts
@@ -537,7 +537,7 @@ async repostajeimpresionsec(object, ID_Repostaje) {
   async neumaticosespesorpresion(object, ID_Vehiculo) {
    const query = `select id_neumatico, identificacion, validar_identificacion_neu, dot, 
    serial_fabricacion, serial_fabricacion_mostrar, semana_fabricacion, anio_fabricacion, 
-   neumatico_modelo_descripcion,neumatico_diseno_descripcion,neumatico_carcasa_naturaleza,
+   neumatico_modelo_descripcion,neumatico_diseno_descripcion, uso_descripcion, neumatico_carcasa_naturaleza,
 fecha_adquisicion, kilometraje, espesor, fecha_act_espesor, ESPESOR_USUARIO_MOD,
 ESPESOR_FECHA_MOD, espesor_observaciones, PRESION,FECHA_ACT_PRESION, PRESION_USUARIO_MOD,
 PRESION_FECHA_MOD, presion_observaciones, fecha_reencauchado, REENCAUCHADO_VECES, fecha_instalacion, eje, posicion,  

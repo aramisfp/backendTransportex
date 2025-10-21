def56e9b6898d3bab23270fa0966ecd828ccea02
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2025-10-21 13:13:30 -0600

    Update users.array.ts
    Nuevo cliente MENSERCA

--- src/users.array.ts
+++ src/users.array.ts
@@ -183,4 +183,12 @@ export const myArray = [
     UserID: 'usr_movil',
     UserPwd: 'Tran5p0rt3X_2008tlf',
   },    
+ {
+    name: 'MENSERCA',
+    Server: 'ec2-3-138-157-245.us-east-2.compute.amazonaws.com',
+    Port: '1433',
+    Database: 'DB_TRANSPORTEX_Menserca',
+    UserID: 'usr_movil',
+    UserPwd: 'Tran5p0rt3X_2008tlf',
+  },      
 ];

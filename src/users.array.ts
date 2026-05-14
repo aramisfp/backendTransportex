20368655dc157b8e8e38b86de69fac9f83cc9763
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2026-05-14 09:49:12 -0600

    Update users.array.ts
    Correcion cliente Klin

--- src/users.array.ts
+++ src/users.array.ts
@@ -192,7 +192,7 @@ export const myArray = [
     UserPwd: 'Tran5p0rt3X_2008tlf',
   },    
    {
-    name: 'KLIN_GUA',
+    name: 'KLIN_ECU',
     Server: 'ec2-18-220-185-41.us-east-2.compute.amazonaws.com',
     Port: '1433',
     Database: 'DB_TRANSPORTEX_KLIN',

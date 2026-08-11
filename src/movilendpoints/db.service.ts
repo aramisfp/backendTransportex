d4bdac72e0bb7d44455a4af5bdce80ff2b2824ec
Aramis Ferrer <39105620+aramisfp@users.noreply.github.com>
2026-08-10 20:52:24 -0600

    Update db.service.ts
    correcion de x.id_empresa registro en repostajes

--- src/movilendpoints/db.service.ts
+++ src/movilendpoints/db.service.ts
@@ -300,7 +300,7 @@ from dbo.F_SEL_VEHICULO_REPOSTAJE (${ID_Empleado}, ${ID_Empresa_Sesion}) as x, c
 	where config_mostrarcosto.campo = 'VEH_COMB3PULG_SHOWCOSTOS'), 1) as config_mostrarcostos )
 	 as cfg_mostrarcostos
  
-where config_showprint3p.campo = 'IMPRESION_MOSTRAR_PDV' and CONFIGURACION_X_EMPRESA.ID_EMPRESA = x.id_empresa_registro
+where config_showprint3p.campo = 'IMPRESION_MOSTRAR_PDV' and CONFIGURACION_X_EMPRESA.ID_EMPRESA = ${ID_Empresa_Sesion}
 order by fecha_reposteo desc , 
 id_vehiculo_combustible desc`;
     const result = await this.general(object, query);
